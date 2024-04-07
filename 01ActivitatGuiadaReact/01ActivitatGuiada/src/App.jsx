30177ebe68f4cbe8a2f44c2d6f9d6b6a2536cb63
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2024-04-07 20:52:05 +0200

    Activitat 1 guiada feta

---
+++ 01ActivitatGuiadaReact/01ActivitatGuiada/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Auth/Login';
+import Register from './Pages/Auth/Register';
+import Header from './components/nav/Header';
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Header />}>
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+    </Route>
+  )
+)
+
+function App({routes}) {
+
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  );
+}
+
+export default App;
